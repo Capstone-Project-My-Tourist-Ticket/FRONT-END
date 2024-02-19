0e4ca6733c57406ae7f02cc6f8494384402f4f31
rfangki <rachmanfangki@gmail.com>
2024-02-19 13:52:53 +0700

    update

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -6,15 +6,16 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { Card, CardContent } from "./ui/card";
-import {  UserReview } from "@/utils/apis/user/type";
+import { UserReview } from "@/utils/apis/user/type";
 
 interface Props {
-  data : UserReview[]
+  data: UserReview[];
 }
 
 const ReviewsComp = (props: Props) => {
-  const {data} = props;
-  const currentDate = (created_at: string)  => new Date(created_at).toDateString();
+  const { data } = props;
+  const currentDate = (created_at: string) =>
+    new Date(created_at).toDateString();
   return (
     <Carousel
       opts={{
@@ -24,24 +25,30 @@ const ReviewsComp = (props: Props) => {
       className="w-full"
     >
       <CarouselContent>
-        {data && data.map((item, index) => (
-          <CarouselItem className="basis-1/4" key={index}>
-            <Card className="mb-8">
-              <CardContent className="p-1 border-2 rounded-lg shadow-lg">
-                <div className="flex-col p-2 space-y-1">
-                  <div className="flex justify-between">
+        {data &&
+          data.map((item, index) => (
+            <CarouselItem className="basis-1/4" key={index}>
+              <Card className="mb-8">
+                <CardContent className="p-1 border-2 rounded-lg shadow-lg">
+                  <div className="flex-col p-2 space-y-1">
+                    <div className="flex justify-between">
+                      <p className="font-semibold text-lg">
+                        {item.start_rate.toFixed(1)}{" "}
+                        <span className="text-slate-500">/5.0</span>
+                      </p>
+                      <p>{currentDate(item.created_at)}</p>
+                    </div>
                     <p className="font-semibold text-lg">
-                      {item.start_rate.toFixed(1)} <span className="text-slate-500">/5.0</span>
+                      {item.user.full_name}
                     </p>
-                    <p>{currentDate(item.created_at)}</p>
+                    <p className=" text-slate-500 overflow-ellipsis min-h-14 max-h-16 overflow-hidden">
+                      {item.text_review}
+                    </p>
                   </div>
-                  <p className="font-semibold text-lg">{item.user.full_name}</p>
-                  <p className=" text-slate-500">{item.text_review}</p>
-                </div>
-              </CardContent>
-            </Card>
-          </CarouselItem>
-        ))}
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
